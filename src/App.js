c6c79a72ceb61b7b9b254928977ae1598e463896
Andrea Calvario <andrea.calvario@konicaminolta.it>
2022-03-01 16:24:09 +0100

    added the board

--- src/App.js
+++ src/App.js
@@ -5,6 +5,35 @@ class ZombiePlagueClient {
   constructor() {
     this.client = Client({ game: ZombiePlague, numPlayers: 2 });
     this.client.start();
+    this.rootElement = document.getElementById("app");
+    this.createBoard();
+  }
+
+  createBoard() {
+    let boardgame = this.client.initialState.G.cells;
+    const rows = [];
+    for (let i = 0; i < boardgame.length; i++) {
+      const cells = [];
+      for (let j = 0; j < boardgame[i].length; j++) {
+        const id = 3 * i + j;
+        cells.push(`<td class="cell" data-id="${id}"></td>`);
+      }
+      rows.push(`<tr>${cells.join('')}</tr>`);
+    }
+
+    this.rootElement.innerHTML = `
+    <style>
+      .cell {
+        border: 1px solid #555;
+        width: 41px;
+        height: 41px;
+        line-height: 50px;
+        text-align: center;
+      }
+    </style>
+      <table style="border-spacing: 0px">${rows.join('')}</table>
+      <p class="winner"></p>
+    `;
   }
 }
 
